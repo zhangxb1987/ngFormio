3147c13b6a36cbd21c3649f4c0d0ea537a0a76a8
zhangxb1987 <332977094@qq.com>
2017-07-06 13:52:54 +0800

    345

---
+++ src/components/chinaaddress.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+
+module.exports = function(app) {
+  app.config([
+    'formioComponentsProvider','$log',
+    function(formioComponentsProvider,$log) {
+      $log.debug('chinaaddress plugin');
+      formioComponentsProvider.register('chinaaddress', {
+        title: 'China Address',
+        template: 'formio/components/china-address.html',
+        controller: ['$scope', '$http', function($scope, $http) {
+          if ($scope.builder) return;
+          $scope.address = {};
+          $scope.addresses = [];
+          $scope.refreshAddress = function(address) {
+            var params = {
+              address: address,
+              sensor: false
+            };
+            if (!address) {
+              return;
+            }
+            if ($scope.component.map && $scope.component.map.region) {
+              params.region = $scope.component.map.region;
+            }
+            if ($scope.component.map && $scope.component.map.key) {
+              params.key = $scope.component.map.key;
+            }
+            return $http.get(
+              'https://maps.googleapis.com/maps/api/geocode/json',
+              {
+                disableJWT: true,
+                params: params,
+                headers: {
+                  Authorization: undefined,
+                  Pragma: undefined,
+                  'Cache-Control': undefined
+                }
+              }
+            ).then(function(response) {
+              $scope.addresses = response.data.results;
+            });
+          };
+        }],
+        tableView: function(data) {
+          return data ? data.formatted_address : '';
+        },
+        group: 'advanced',
+        settings: {
+          input: true,
+          tableView: true,
+          label: '',
+          key: 'chinaAddressField',
+          placeholder: '',
+          multiple: false,
+          protected: false,
+          clearOnHide: true,
+          unique: false,
+          persistent: true,
+          hidden: false,
+          map: {
+            region: '',
+            key: ''
+          },
+          validate: {
+            required: false
+          }
+        }
+      });
+    }
+  ]);
+  app.run([
+    '$templateCache',
+    function($templateCache) {
+      $templateCache.put('formio/components/china-address.html',
+        fs.readFileSync(__dirname + '/../templates/components/address.html', 'utf8')
+      );
+    }
+  ]);
+};
